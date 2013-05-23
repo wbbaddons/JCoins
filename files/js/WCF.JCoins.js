af8f186e1dfe630c4bdf6ab4f5df39cbda7673af
Dennis Kraffczyk <dennis.kraffczyk@googlemail.com>
2013-05-23 23:17:12 +0200

    Added documentation of the WCF.JCoins.js

--- files/js/WCF.JCoins.js
+++ files/js/WCF.JCoins.js
@@ -1,13 +1,32 @@
+/**
+ * WCF.JCoins is a class, which contains a collection of functions for
+ * the 'de.joshsboard.jCoins'-package.
+ * 
+ * @author 		Joshua Ruesweg
+ * @copyright	JoshsBoard 2013
+ * @package		de.joshsboard.jCoins	
+ */
 WCF.JCoins = { };
 
+/**
+ * Handles clicks on 'buyPremiumGroupButton'.
+ */
 WCF.JCoins.Buy = Class.extend({
 	/**
 	 * action proxy
 	 * @var	WCF.Action.Proxy
 	 */
 	_proxy: null,
+	
+	/**
+	 * id of premium-group
+	 * @var	integer
+	 */
 	_premiumGroupID: null,
 	
+	/**
+	 * Creates a new object of this class.
+	 */
 	init: function(premiumGroupID) {
 		this._premiumGroupID = premiumGroupID;
 		
@@ -16,10 +35,14 @@ WCF.JCoins.Buy = Class.extend({
 			showLoadingOverlay: true,
 			success: $.proxy(this._success, this)
 		});
-                
+		
+		// add eventlistener
 		$('#buyPremiumGroupButton'+premiumGroupID).click($.proxy(this._click, this));
 	},
 	
+	/**
+	 * Sends request after clicking on a button.
+	 */
 	_click: function(event) {
 		this._proxy.setOption('data', {
 			actionName: 'buyGroup',
@@ -30,13 +53,25 @@ WCF.JCoins.Buy = Class.extend({
 		this._proxy.sendRequest();
 	},
 	
+	/**
+	 * Shows a notification on success.
+	 * @param	object		data
+	 * @param	string		textStatus
+	 * @param	jQuery		jqXHR
+	 */
 	_success: function(data, textStatus, jqXHR) {
-		var $notification = new WCF.System.Notification('data.returnValues.successMessage');
+		var $notification = new WCF.System.Notification(data.returnValues.successMessage);
 		$notification.show();
 	},
 	
+	/**
+	 * Shows a notification on failure.
+	 * @param	object		data
+	 * @param	string		textStatus
+	 * @param	jQuery		jqXHR
+	 */
 	_failure: function(data, textStatus, jqXHR) {
-		var $notification = new WCF.System.Notification('data.returnValues.falseMessage');
+		var $notification = new WCF.System.Notification(data.returnValues.falseMessage);
 		$notification.show();
 	}
 });
