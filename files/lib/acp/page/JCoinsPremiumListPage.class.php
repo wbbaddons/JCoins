47db6be1ffa7963506b299a2adf172acf70cf3cb
Joshua Rüsweg <josh@joshsboard.de>
2013-12-16 00:04:35 +0100

    Update JCoinsPremiumListPage.class.php

--- files/lib/acp/page/JCoinsPremiumListPage.class.php
+++ files/lib/acp/page/JCoinsPremiumListPage.class.php
@@ -50,7 +50,7 @@ class JCoinsPremiumListPage extends SortablePage {
 	protected function initObjectList() {
 		parent::initObjectList();
 
-		$this->objectList->sqlSelects .= "(SELECT COUNT(*) FROM wcf" . WCF_N . "_user_to_group_premium WHERE premiumGroupID = user_group_premium.premiumGroupID) AS members";
+		$this->objectList->sqlSelects .= "(SELECT COUNT(*) FROM wcf" . WCF_N . "_user_to_group_temp WHERE groupID = user_group_premium.premiumGroupID) AS members";
 	}
 
 	/**
