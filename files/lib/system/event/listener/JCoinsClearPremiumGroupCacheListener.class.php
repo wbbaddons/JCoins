b55dea5dd0ad260dc9c6261a715ed789e1c3428b
joshuaruesweg <josh@joshsboard.de>
2014-02-06 17:46:21 +0100

    fix http://joshsboard.de/index.php/Thread/201-User-aus-Premiumgruppe-entfernen/?postID=1013#post1013

---
+++ files/lib/system/event/listener/JCoinsClearPremiumGroupCacheListener.class.php
@@ -0,0 +1,25 @@
+<?php
+namespace wcf\system\event\listener;
+
+use wcf\system\event\IEventListener;
+use wcf\system\user\storage\UserStorageHandler;
+
+/**
+ * clear the premium cache
+ * 
+ * @author	Joshua Rüsweg
+ * @package	de.joshsboard.jcoins
+ */
+class JCoinsClearPremiumGroupCacheListener implements IEventListener {
+
+	/**
+	 * @see	wcf\system\event\IEventListener::execute()
+	 */
+	public function execute($eventObj, $className, $eventName) {
+		if (count($eventObj->user) == 0) return; 
+		
+		// reset storage
+		UserStorageHandler::getInstance()->reset(array(array_keys($eventObj->userToGroups)), 'jCoinsPremiumGroupIDs');
+	}
+
+}
