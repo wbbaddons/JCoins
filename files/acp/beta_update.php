a2e238d48278368618720eb233433049886efedd
joshuaruesweg <josh@joshsboard.de>
2014-08-29 18:07:53 +0200

    set real WCF_N

--- files/acp/beta_update.php
+++ files/acp/beta_update.php
@@ -1,10 +1,10 @@
 <?php
 // sql pipe doesn't support renaming
-$stmt = \wcf\system\WCF::getDB()->prepareStatement("ALTER TABLE wcf1_jcoins_shop_item_type RENAME COLUMN isMultible TO isMultiple;");
+$stmt = \wcf\system\WCF::getDB()->prepareStatement("ALTER TABLE wcf". WCF_N ."_jcoins_shop_item_type RENAME COLUMN isMultible TO isMultiple;");
 $stmt->execute();
 
 $stmt = \wcf\system\WCF::getDB()->prepareStatement("
-ALTER TABLE  wcf1_user_jcoins_statement ADD (
+ALTER TABLE  wcf". WCF_N ."_jcoins_shop_item ADD (
 	parseBBCodes BOOLEAN NOT NULL DEFAULT 1, 
 	allowHTML BOOLEAN NOT NULL DEFAULT 0, 
 	allowSmileys BOOLEAN NOT NULL DEFAULT 0
