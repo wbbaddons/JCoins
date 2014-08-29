0477364a1861b74cfec78c98eaeae27a95d2e35a
joshuaruesweg <josh@joshsboard.de>
2014-08-29 18:01:57 +0200

    unsupported sql-query to beta_update.php

--- beta_update.sql
+++ beta_update.sql
@@ -1,7 +1,1 @@
 ALTER TABLE  wcf1_user_jcoins_statement ADD additionalData MEDIUMTEXT;
-
-ALTER TABLE  wcf1_user_jcoins_statement ADD (
-	parseBBCodes BOOLEAN NOT NULL DEFAULT 1, 
-	allowHTML BOOLEAN NOT NULL DEFAULT 0, 
-	allowSmileys BOOLEAN NOT NULL DEFAULT 0
-);
--- files/acp/beta_update.php
+++ files/acp/beta_update.php
@@ -2,3 +2,11 @@
 // sql pipe doesn't support renaming
 $stmt = \wcf\system\WCF::getDB()->prepareStatement("ALTER TABLE wcf1_jcoins_shop_item_type RENAME COLUMN isMultible TO isMultiple;");
 $stmt->execute();
+
+$stmt = \wcf\system\WCF::getDB()->prepareStatement("
+ALTER TABLE  wcf1_user_jcoins_statement ADD (
+	parseBBCodes BOOLEAN NOT NULL DEFAULT 1, 
+	allowHTML BOOLEAN NOT NULL DEFAULT 0, 
+	allowSmileys BOOLEAN NOT NULL DEFAULT 0
+)");
+$stmt->execute(); 
