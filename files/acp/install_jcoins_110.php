1c5a7081537803221dfe17320e973f2498e8f32d
joshuaruesweg <josh@joshsboard.de>
2014-08-29 13:55:45 +0200

    fix update

--- files/acp/install_jcoins_110.php
+++ files/acp/install_jcoins_110.php
@@ -1,4 +1,7 @@
 <?php
+$sql = "UPDATE wcf". WCF_N ."_user_jcoins_statement SET additionalData = ?";
+$stmt = wcf\system\WCF::getDB()->prepareStatement($sql); 
+$stmt->execute(array('a:0:{}'));
 
 \wcf\system\dashboard\DashboardHandler::setDefaultValues('com.woltlab.wcf.user.MembersListPage', array(
 	'de.joshsboard.jcoins.richestUser' => 1
